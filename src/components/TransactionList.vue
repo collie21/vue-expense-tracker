bb0aa9ee643725a9a3d18245f9d971a582c858d3
user <collie@hotmail.co.uk>
2024-02-16 12:57:17 -0400

    Vue Expense Tracker

---
+++ src/components/TransactionList.vue
@@ -0,0 +1,59 @@
+<template>
+  <h3>History</h3>
+  <ul id="list" class="list">
+    <li
+      v-for="transaction in transactions"
+      :key="transaction.id"
+      :class="transaction.amount < 0 ? 'minus' : 'plus'"
+    >
+      {{ transaction.text }} <span>${{ transaction.amount }}</span
+      ><button class="delete-btn" @click="deleteTransaction(transaction.id)">
+        x
+      </button>
+    </li>
+  </ul>
+</template>
+
+<script setup>
+import { defineProps } from "vue";
+
+const emit = defineEmits(["transactionDeleted"]);
+
+const props = defineProps({
+  transactions: {
+    type: Array,
+    required: true,
+  },
+});
+
+//  Delete Transaction
+const deleteTransaction = (id) => {
+  emit("transactionDeleted", id);
+};
+</script>
+
+<!-- <script>
+// options API
+export default {
+  data() {
+    return {
+      transactions: [
+        { id: 1, text: "Flower", amount: -19.99 },
+        { id: 2, text: "Salary", amount: 299.97 },
+        { id: 3, text: "Book", amount: -10 },
+        { id: 4, text: "Camera", amount: 150 },
+      ],
+    };
+  },
+};
+</script> -->
+<!-- <script setup>
+// composition API
+
+const transactions = [
+  { id: 1, text: "Flower", amount: -19.99 },
+  { id: 2, text: "Salary", amount: 299.97 },
+  { id: 3, text: "Book", amount: -10 },
+  { id: 4, text: "Camera", amount: 150 },
+];
+</script> -->
